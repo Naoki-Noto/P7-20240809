a81b43fbe07a90ed1f5a9d24846e8ac7b19b922b
Naoki N. <118686232+Naoki-Noto@users.noreply.github.com>
2024-10-22 11:55:07 +0900

    Add files via upload

---
+++ Supporting information/Removing_duplicate/data_Random/Make_database_adapt1/source/duplicated_mols_check.py
@@ -0,0 +1,34 @@
+# -*- coding: utf-8 -*-
+"""
+Created on Tue Mar 26 10:03:43 2024
+
+@author: noton
+"""
+
+import pandas as pd
+import numpy as np
+from rdkit import Chem
+
+data = pd.read_csv('Human_made_smiles.csv')
+print(data)
+duplicated_smiles = data[data.duplicated(subset='SMILES')]
+print(duplicated_smiles)
+duplicated_smiles.to_csv('Human_made_smiles_dp.csv', index=False)
+
+data_unique = data.drop_duplicates(subset='SMILES')
+print(data_unique)
+data_unique.to_csv('Human_made_smiles_2.csv', index=False)
+
+smiles_list = np.array(data['SMILES'])
+mols = [Chem.MolFromSmiles(smiles) for smiles in smiles_list]
+
+unique_mols = {}
+for mol in mols:
+    canonical_smiles = Chem.MolToSmiles(mol)
+    unique_mols[canonical_smiles] = mol
+
+final_smiles_list = list(unique_mols.keys())
+
+f_smiles = pd.DataFrame(final_smiles_list, columns=['SMILES'])
+print(f_smiles)
+f_smiles.to_csv('Human_made_smiles_3.csv', index=False)
